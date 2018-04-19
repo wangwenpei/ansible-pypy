724dae10f74fb08fb09fc4d8bb9f6c18a8023a96
WANG WENPEI <wangwenpei@nextoa.com>
2018-04-19 20:25:40 +0800

    fix bugs

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -4,24 +4,27 @@ set -e
 
 cd
 
-PYPY_VERSION={{ pypy_version }}
-PYPY_ARCH={{ pypy_arch }}
+PYPY_VERSION=$1
+PYPY_ARCH=$2
+PYPY_BIN_PATH=$3
+PYPY_HOME=$4
+PYPY_MIRROR=$5
 
 if [[ -e $HOME/pypy$PYPY_VERSION-$PYPY_ARCH.tar.bz2 ]]; then
   tar -xjf $HOME/pypy$PYPY_VERSION-$PYPY_ARCH.tar.bz2
   rm -rf $HOME/pypy$PYPY_VERSION-$PYPY_ARCH.tar.bz2
 else
-  wget --no-check-certificate -O - {{pypy_mirror}}/pypy$PYPY_VERSION-$PYPY_ARCH.tar.bz2 | tar -xjf -
+  wget --no-check-certificate -O - $PYPY_MIRROR/pypy$PYPY_VERSION-$PYPY_ARCH.tar.bz2 | tar -xjf -
 fi
 
-mv -n pypy$PYPY_VERSION-$PYPY_ARCH {{ pypy_home }}
-test -d {{ pypy_bin_path }} || mkdir -p {{ pypy_bin_path }}
+mv -n pypy$PYPY_VERSION-$PYPY_ARCH $PYPY_HOME
+test -d $PYPY_BIN_PATH || mkdir -p $PYPY_BIN_PATH
 
-cat > {{pypy_bin_path}}/python <<EOF
+cat > $PYPY_BIN_PATH/python <<EOF
 #!/usr/bin/env bash
-LD_LIBRARY_PATH=$HOME/pypy/lib:/usr/lib64:/lib64:/usr/lib:/lib:$LD_LIBRARY_PATH exec {{pypy_home}}/bin/pypy "\$@"
+LD_LIBRARY_PATH=$PYPY_HOME/lib:/usr/lib64:/lib64:/usr/lib:/lib:$LD_LIBRARY_PATH exec $PYPY_HOME/bin/pypy "\$@"
 EOF
 
-chmod +x {{pypy_bin_path}}
-{{pypy_bin_path}}/python --version
+chmod +x $PYPY_BIN_PATH
+$PYPY_BIN_PATH/python --version
 
